51c093635823bebf977d99f1326a5619dd84aed2
Carlos Marinho <carluizfla@hotmail.com>
2020-11-14 19:40:32 -0300

    correctin linting errors 3

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -2,9 +2,8 @@ from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 
-class AdminSiteTest(TestCase):
-    
 
+class AdminSiteTest(TestCase):
     def setUp(self):
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
